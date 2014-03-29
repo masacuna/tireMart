bc109290147ee7f6fe17def953e2065028ea7b76
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-29 10:40:37 -0500

    First commit for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TireMart::Application.config.secret_key_base = '0556d74ba6f6cf0d2bc1553e70e24e1fe9219476190a31c225023a313a2d9ca065c32058d462b67a1e9d62eeefde948b30b54a17bfae71e68754f2aa5632e0bb'
