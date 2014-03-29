656165a5e8f20b0ac8c2d6d371c645b9e8d38430
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-29 14:17:58 -0500

    Added to the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 TireMart::Application.routes.draw do
-  get 'products', to: 'products#index', as: 'products'
+  
+
 
   # get "products/index"
   # get "products/show"
@@ -13,7 +14,7 @@ TireMart::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
