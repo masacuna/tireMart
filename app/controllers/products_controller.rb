c65b69810a11f2ea49bf0c107ec5157c7384fdbb
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-30 12:08:01 -0500

    Enter in more test product for the create product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,14 +6,20 @@ class ProductsController < ApplicationController
   # app/views/products/index.html.erb
 
   def show
-    @products = Product.find(params[:id])
+    @product = Product.find(params[:id])
   end
 
   def new
-     
+     @product = Product.new
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   def edit
@@ -24,4 +30,9 @@ class ProductsController < ApplicationController
 
   def destroy
   end
-end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :description,:stock_quantity)
+    end
+end 
