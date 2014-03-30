0857f32b37ec9bc5021f787748ada72a19145432
Dennis Acuna <dacuna30@academic.rrc.ca>
2014-03-30 13:09:07 -0500

    Implemented update and edit for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+	validates :name, :price, :stock_quantity, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :stock_quantity, numericality: {only_integer: true}
 end
